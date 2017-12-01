0444c20e9a480bd469dad21e0a15490e1f709c65
Maria Princi <maria.souza_princi@epita.fr>
2017-12-02 00:37:47 +0100

    vector.h: structure vector2 as parameters are no longer *

--- src/vector/vector.h
+++ src/vector/vector.h
@@ -1,20 +1,22 @@
 #ifndef VECTOR_H
 # define VECTOR_H
 
+#include <stdlib.h>
+
 struct vector2
 {
   float x;
   float y;
 };
 
-struct vector2 *vector_create(void);
+struct vector2 *create_vect(void);
 
-void vector_destroy(struct vector2 *a);
+void destroy_vect(struct vector2 *v);
 
-struct vector2 *add_vect(struct vector2 *a, struct vector2 *b);
+struct vector2 *add_vect(struct vector2 a, struct vector2 b); 
 
-struct vector2 *mult_two_vects(struct vector2 *a, struct vector2 *b);
+struct vector2 *mult_two_vects(struct vector2 a, struct vector2 b); 
 
-struct vector2 *mult_vect_coef(struct vector2 *a, float coef);
+struct vector2 *mult_vect_coef(struct vector2 a, float coef);
 
 #endif /* !VECTOR_H */
