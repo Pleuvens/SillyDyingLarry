9d0c47f31f3430b7974e3be99c67e04ba315bec7
Maria Princi <maria.souza_princi@epita.fr>
2017-12-02 00:36:44 +0100

    character.c: create, destroy and move character prototypes added

--- src/character/character.h
+++ src/character/character.h
@@ -1,6 +1,9 @@
 #ifndef CHARACTER_H
 # define CHARACTER_H
 
+#define MAX_POSX 10
+#define MAX_POSY 10
+
 enum state
 {
   ALIVE = 0,
@@ -12,5 +15,11 @@ struct character
   struct vector2 *pos;
   enum state state;
 };
+
+struct character *character_create(void);
+
+void character_destroy(struct character *c);
+
+int character_move(struct character *c, struct vector2 new_pos);
 
 #endif /* !CHARACTER_H */
