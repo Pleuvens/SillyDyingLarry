24a61339394f1af472ec5643c57f7c23f128e095
Maria Princi <maria.souza_princi@epita.fr>
2017-12-02 00:38:10 +0100

    vector.c: structure vector2 as parameters are no longer *

--- src/vector/vector.c
+++ src/vector/vector.c
@@ -1,7 +1,6 @@
-#include <stdlib.h>
 #include "vector.h"
 
-struct vector2 *vector_create(void)
+struct vector2 *create_vect(void)
 {
   struct vector2 *new = calloc(1, sizeof(struct vector2));
   if (!new)
@@ -9,31 +8,41 @@ struct vector2 *vector_create(void)
   return new;
 }
 
-void vector_destroy(struct vector2 *a)
+void destroy_vect(struct vector2 *v)
 {
-  free(a);
+  if (!v)
+    return;
+  free(v);
 }
 
-struct vector2 *add_vect(struct vector2 *a, struct vector2 *b)
+struct vector2 *add_vect(struct vector2 a, struct vector2 b)
 {
-  struct vector2 *res = vector_create();
-  res->x = a->x + b->x;
-  res->y = a->y + b->y;
+  struct vector2 *res = create_vect();
+
+  if (!res)
+    return NULL;
+  res->x = a.x + b.x;
+  res->y = a.y + b.y;
+  return res;
+}
+struct vector2 *mult_two_vects(struct vector2 a, struct vector2 b)
+{
+  struct vector2 *res = create_vect();
+
+  if (!res)
+    return NULL;
+  res->x = a.x * b.x;
+  res->y = a.y * b.y;
   return res;
 }
 
-struct vector2 *mult_two_vects(struct vector2 *a, struct vector2 *b)
+struct vector2 *mult_vect_coef(struct vector2 a, float coef)
 {
-  struct vector2 *res = vector_create();
-  res->x = a->x * b->x;
-  res->y = a->y * b->y;
-  return res;
-}
+  struct vector2 *res = create_vect();
 
-struct vector2 *mult_vect_coef(struct vector2 *a, float coef)
-{
-  struct vector2 *res = vector_create();
-  res->x = a->x * coef;
-  res->y = a->y * coef;
+  if (!res)
+    return NULL;
+  res->x = a.x * coef;
+  res->y = a.y * coef;
   return res;
 }
