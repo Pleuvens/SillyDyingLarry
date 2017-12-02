cc3cd69fa02b05c7dced965533174759715650a2
Maria Princi <maria.souza_princi@epita.fr>
2017-12-02 01:56:30 +0100

    character.h: move_character takes context instead of player as param

--- src/character/character.h
+++ src/character/character.h
@@ -2,21 +2,10 @@
 # define CHARACTER_H
 
 #include <SDL2/SDL.h>
+#include "../structures.h"
 
 #define MAX_POSX 10
 #define MAX_POSY 10
-
-enum state
-{
-  ALIVE = 0,
-  DEAD
-};
-
-struct character
-{
-  struct vector2 *pos;
-  enum state state;
-};
 
 struct character *character_create(void);
 
@@ -24,6 +13,6 @@ void character_destroy(struct character *c);
 
 int character_move(struct character *c, struct vector2 new_pos);
 
-void move_character(struct character *c, SDL_Event e);
+void move_character(struct context *c, SDL_Event e);
 
 #endif /* !CHARACTER_H */
