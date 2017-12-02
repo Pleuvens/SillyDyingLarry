99921866650ff0f9ee55979ba6b7d4d01baf12d1
Maria Princi <maria.souza_princi@epita.fr>
2017-12-02 01:31:03 +0100

    move.c: vector.h include added and more coding style file

--- src/character/move.c
+++ src/character/move.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+#include "../vector/vector.h"
 #include "character.h"
 
 #define MOVE_SIZE 10
 
 void move_character(struct character *c, SDL_Event e)
 {
-  else if(e.type == SDL_KEYDOWN)
+  if (e.type == SDL_KEYDOWN)
   {
-    switch(e.key.keysym.sym)
+    switch (e.key.keysym.sym)
     {
       case SDLK_UP:
       {
