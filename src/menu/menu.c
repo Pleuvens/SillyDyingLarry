0ff4e10046bc5cd957f1c0b453eb2d9ffbb23cb1
Thibault Guisnet <thibault.guisnet@epita.fr>
2017-12-02 01:12:55 +0100

    [MENU] add feature for the menu

--- src/menu/menu.c
+++ src/menu/menu.c
@@ -1,55 +1,98 @@
 //Using SDL and standard IO
-#include <SDL2/SDL2/SDL.h>
+#include <SDL2/SDL.h>
 #include <stdio.h>
 
 //Screen dimension constants
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 608;
 
-int main( int argc, char* args[] )
-{
-    //The window we'll be rendering to
-    SDL_Window* window = NULL;
+//The window we'll be rendering to
+SDL_Window* window = NULL;
     
-    //The surface contained by the window
-    SDL_Surface* screenSurface = NULL;
+//The surface contained by the window
+SDL_Surface* screenSurface = NULL;
 
-    //Initialize SDL
-    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+//The image we will load and show on the screen
+SDL_Surface* test = NULL;
+
+int initialisation(void)
+{
+  int res = 0;
+
+  //Initialize SDL
+  if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+  {
+    printf("SDL initialisation, Error: %s\n", SDL_GetError());
+  }
+  else
+  {
+    //Create window
+    window = SDL_CreateWindow("Silly Dying Larry", 0, 0, SCREEN_WIDTH, 
+                              SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if( window == NULL )
     {
-        printf("SDL initialisation, Error: %s\n", SDL_GetError());
+      printf("Window creation, Error: %s\n", SDL_GetError());
     }
     else
     {
-        //Create window
-        window = SDL_CreateWindow("Silly Dying Larry", SDL_WINDOWPOS_UNDEFINED, 
-                                  SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, 
-                                  SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-        if( window == NULL )
-        {
-            printf("Window creation, Error: %s\n", SDL_GetError());
-        }
-        else
-        {
-            //Get window surface
-            screenSurface = SDL_GetWindowSurface(window);
-
-            //Fill the surface white
-            SDL_FillRect(screenSurface, NULL, 
-                         SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
-            
-            //Update the surface
-            SDL_UpdateWindowSurface(window);
-
-            //Wait two seconds
-            SDL_Delay(2000);
-        }
+      //Get window surface
+      screenSurface = SDL_GetWindowSurface(window);
     }
-    //Destroy window
-    SDL_DestroyWindow( window );
+  }
 
-    //Quit SDL subsystems
-    SDL_Quit();
+  return res;
+}
 
-    return 0;
+int load_image(void)
+{
+	int res = true;
+
+	//Load splash image
+	test = SDL_LoadBMP( "02_getting_an_image_on_the_screen/hello_world.bmp" );
+	if( gHelloWorld == NULL )
+	{
+		printf( "Unable to load image %s! SDL Error: %s\n", "02_getting_an_image_on_the_screen/hello_world.bmp", SDL_GetError() );
+		success = false;
+	}
+
+	return res;
+}
+
+void close()
+{
+	//Deallocate surface
+	SDL_FreeSurface( gHelloWorld );
+	gHelloWorld = NULL;
+
+	//Destroy window
+	SDL_DestroyWindow( gWindow );
+	gWindow = NULL;
+
+	//Quit SDL subsystems
+	SDL_Quit();
+}
+
+int main(void)
+{
+
+
+
+      //Fill the surface white
+      SDL_FillRect(screenSurface, NULL, 
+                   SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+            
+      //Update the surface
+      SDL_UpdateWindowSurface(window);
+
+      //Wait two seconds
+      SDL_Delay(2000);
+    }
+  }
+  //Destroy window
+  SDL_DestroyWindow(window);
+
+  //Quit SDL subsystems
+  SDL_Quit();
+  close();
+  return 0;
 }
