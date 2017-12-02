7cba6d9eda0c04347231829fa8c27c127b84cd6d
Maria Princi <maria.souza_princi@epita.fr>
2017-12-02 02:50:49 +0100

    move.c: character has now gravity

--- src/character/move.c
+++ src/character/move.c
@@ -3,31 +3,56 @@
 #include "../vector/vector.h"
 
 #define MOVE_SIZE 1
+#define GRAVITY 1
 
 void move_character(struct context *c, SDL_Event e)
 {
   if (e.type == SDL_KEYDOWN)
   {
+    printf("before: x = %f y = %f\n",
+          c->player->pos->x, c->player->pos->y);
+    float y = c->player->pos->y;
+    float x = c->player->pos->x;
+    float speed = c->player->speed;
     switch (e.key.keysym.sym)
     {
       case SDLK_UP:
       {
-        if (c->player->pos->y - MOVE_SIZE >= 0)
-          c->player->pos->y -= MOVE_SIZE;
+        if (y - MOVE_SIZE >= 0
+            && c->map->type[(int)(y-MOVE_SIZE)*c->map->width + (int)x] == NONE)
+          c->player->pos->y -= 2*MOVE_SIZE;
         break;
       }
       case SDLK_RIGHT:
       {
-        if (c->player->pos->x + MOVE_SIZE < c->map->width)
-          c->player->pos->x += MOVE_SIZE;
+        if (x + MOVE_SIZE < c->map->width
+            && c->map->type[(int)y*c->map->width + (int)(x+MOVE_SIZE)] == NONE)
+          c->player->pos->x += speed;
         break;
       }
       case SDLK_LEFT:
       {
-        if (c->player->pos->x - MOVE_SIZE >= 0)
-          c->player->pos->x -= MOVE_SIZE;
+        if (x - MOVE_SIZE >= 0
+            && c->map->type[(int)y*c->map->width + (int)(x-MOVE_SIZE)] == NONE)
+          c->player->pos->x -= speed;
+        break;
+      }
+      //Down for tests only
+      case SDLK_DOWN:
+      {
+        if (y + MOVE_SIZE < c->map->height
+            && c->map->type[(int)(y+MOVE_SIZE)*c->map->width + (int)x] == NONE)
+          c->player->pos->y += GRAVITY;
         break;
       }
     }
   }
+  float y = c->player->pos->y;
+  float x = c->player->pos->x;
+
+  if (y + MOVE_SIZE < c->map->height
+    && c->map->type[(int)(y+MOVE_SIZE)*c->map->width + (int)x] == NONE)
+    c->player->pos->y += GRAVITY;
+  printf("after: x = %f y = %f\n",
+          c->player->pos->x, c->player->pos->y);
 }
